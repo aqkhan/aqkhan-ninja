026af3b429f63785845194b11ac0194d629ba343
assadakramiintellect <assad@iintellect.co.uk>
2019-12-03 14:55:28 +0500

    GA Added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import React, { useEffect } from "react";
 import Head from '../components/Head';
 import '../static/style.css';
 import Hero from '../components/Hero';
 import Navigation from '../components/Navigation';
+import { initGA, logPageView } from '../utils/analytics';
 
 export default function index() {
+
+    useEffect(() => {
+        if (!window.GA_INITIALIZED) {
+            initGA();
+            window.GA_INITIALIZED = true
+        }
+        logPageView()
+    });
+
     return (
         <React.Fragment>
             <Head title="Chief Technology Officer" />
