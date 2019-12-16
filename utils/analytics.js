195d696eccdac662d2e85d7e5ccbac6e1ce657d6
assadakramiintellect <assad@iintellect.co.uk>
2019-12-16 21:22:02 +0500

    analytics updated

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga'
 export const initGA = () => {
-    ReactGA.initialize('UA-31896659-2')
+    ReactGA.initialize('UA-154672431-1')
 }
 export const logPageView = () => {
     ReactGA.set({ page: window.location.pathname });
